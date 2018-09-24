c4cef4301e925bf93137057d74c1d01dd6acba9b
yemimasutanto <yemimasutanto@hotmail.com>
2018-09-24 14:10:48 +0700

    change

--- app.py
+++ app.py
@@ -60,19 +60,19 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-    if text=="adit":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat' +text))
-    if text=="mail":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
-    if text=="kana":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Putusin Kana'))
+    
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat '+text))
+    #if text=="mail":
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
+    if text=="kana" or "hans":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu tega, '+text))
     #a=(randint(0, 9))
     #if a%2:
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Iya'))
     #else:
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Tidak'))
 
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\n'+text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\n'+text+' juga :)'))
 
 import os
 if __name__ == "__main__":
