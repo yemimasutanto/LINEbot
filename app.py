2a87f379ca1b1f2fb714047fcc01d01914b88be4
Yemima Sutanto <36811988+yemimasutanto@users.noreply.github.com>
2018-10-14 13:34:21 +0700

    dibenerin mas andika

--- app.py
+++ app.py
@@ -49,6 +49,21 @@ def show(nrp):
     r = requests.get(URLadmin)
     data = r.json()
     err = "Data tidak ditemukan"
+    
+    # flag = data['flag']
+    # if(flag == "1"):
+    #     nrp = data['data_admin'][0]['nrp']
+    #     nama = data['data_admin'][0]['nama']
+    #     alamat = data['data_admin'][0]['alamat']
+
+    #     # munculin semua, ga rapi, ada 'u' nya
+    #     all_data = data['data_admin'][0]
+    #     data= "nama : "+nama+"\nnrp : "+nrp+"\nalamat : "+alamat
+    #     #return data
+    #     return all_data
+
+    # elif(flag == "0"):
+    #     return err
 
 #INPUT DATA ADMIN RPL buat di app.py
 def add(nrp, nama, alamat):
@@ -74,7 +89,7 @@ def listadmin():
             nrp = data['data_admin'][int (i)]['nrp']
             nama = data['data_admin'][int (i)]['nama']
             alamat = data['data_admin'][int (i)]['alamat']
-            print("List admin"):
+            print("List admin")
             hasil=hasil+str(i+1)
             hasil=hasil+".\nNrp : "
             hasil=hasil+nrp
@@ -86,6 +101,9 @@ def listadmin():
         return hasil
     elif(flag == "0"):
         return 'Data gagal dimasukkan\n'
+
+hehe=listadmin();
+print(hehe)
 
 # #DELETE DATA ADMIN RPL
 def delete(nrp):
@@ -148,10 +166,10 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=delete(data[1])))
     elif(data[0]=='replace'):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=update(data[1],data[2],data[3],data[4])))
-    elif(data[0]=='listadmin'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=listadmin()))
+    elif(data[0]=='all'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=alladmin()))
     elif(data[0]=='/menu'):
-        menu = "1. show-[nrp]\n2. add-[nrp]-[nama]-[alamat]\n3. delete-[nrp]\n4. replace-[nrp lama]-[nrp baru]-[nama baru]-[alamat baru]\n5. listadmin "
+        menu = "1. show-[nrp]\n2. add-[nrp]-[nama]-[alamat]\n3. delete-[nrp]\n4. replace-[nrp lama]-[nrp baru]-[nama baru]-[alamat baru]\n5. all admin "
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=menu))
 
 import os
